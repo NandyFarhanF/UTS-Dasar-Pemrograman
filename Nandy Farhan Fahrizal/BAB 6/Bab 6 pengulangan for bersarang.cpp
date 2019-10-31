a968bd13149f93db83a1f15ad2431d8b9ebe1a37
NandyFarhanF <55900789+NandyFarhanF@users.noreply.github.com>
2019-10-31 10:05:49 +0700

    UTS Dasar Pemrograman

---
+++ Nandy Farhan Fahrizal/BAB 6/Bab 6 pengulangan for bersarang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int j=l; j<=10; j++) {
+        for(int k=l; k<=j; k++){
+            cout << k*j << ' ';
+        }
+    cout << "\n";
+    }
+    return 0;
+}
